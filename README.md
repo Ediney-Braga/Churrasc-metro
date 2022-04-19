f50af6fa2c1d6a71ca9d79644804d001cbd97e9b
Ed <contatoedineybraga@gmail.com>
2022-04-19 01:43:41 -0300

    Churracômetro

---
+++ README.md
@@ -0,0 +1,17 @@
+Projeto Churracômetro
+
+Este projeto calcula a qualtidade de mantimentos necessarios para fazer um churrasco basiando-se no numero de adultos, crianças e na duração.
+
+
+
+
+Obs.: Projeto feito para estudos sobre a integração do HTML, CSS e JS.
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,20 @@
+let inputPessoas = document.getElementById("pessoas");
+
+
+
+
+function calcular (){
+    let pessoas = inputPessoas.value;
+    
+    console.log(pessoas);
+}
+
+
+
+
+
+
+
+
+let botao = document.getElementById("botao");
+botao.addEventListener("click", calcular);
